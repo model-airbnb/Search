a7a5c27f6cd7696eaf0eb5b5ff1cb043730c1176
christine-w <christine.wong@gmail.com>
2017-10-24 12:13:19 -0700

    Accept a limit parameter to specify the number of listings to retrieve This commit also includes the unit tests for this change.

--- inventoryLocalStore/index.js
+++ inventoryLocalStore/index.js
@@ -7,13 +7,13 @@ class Inventory {
     });
   }
 
-  getListings(market) {
-    const queryString = `SELECT * FROM listings WHERE market = '${market}'`;
+  getListings(market, limit) {
+    const queryLimit = limit ? ` FETCH FIRST ${limit} ROWS ONLY` : '';
+    const queryString = `SELECT * FROM listings WHERE market = '${market}'${queryLimit}`;
     return this.pool.query(queryString)
       .then(result => result.rows)
       .catch(console.error);
   }
 }
-
 
 module.exports = Inventory;
