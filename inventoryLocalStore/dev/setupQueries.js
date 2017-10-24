f4f194a05598e08a018436a7ee8cfe12dbd5f246
christine-w <christine.wong@gmail.com>
2017-10-23 20:41:00 -0700

    Organize dev setup query strings into a separate module This commit aims to improve readability and modularity of the code.

--- inventoryLocalStore/dev/setupQueries.js
+++ inventoryLocalStore/dev/setupQueries.js
@@ -1,9 +1,3 @@
-// This script tears down all existing database artifacts for the service, sets
-// them up from scratch and populates the tables with some seed data.
-
-const { Pool } = require('pg');
-const { database, pgConnection, dbConnection } = require('./config');
-
 const createTableQueries = {
   listings: `
     CREATE TABLE listings (
@@ -55,10 +49,10 @@ const createTableQueries = {
 
 const csvImportQueries = {
   listings: `
-    COPY listings_raw_data FROM '${__dirname}/starterData/ListingSummaryInformation-SF.csv' DELIMITER ',' CSV HEADER;
+    COPY listings_raw_data FROM '${__dirname}/seedData/ListingSummaryInformation-SF.csv' DELIMITER ',' CSV HEADER;
   `,
   availability: `
-    COPY availability_raw_data FROM '${__dirname}/starterData/Availability-SF.csv' DELIMITER ',' CSV HEADER;
+    COPY availability_raw_data FROM '${__dirname}/seedData/Availability-SF.csv' DELIMITER ',' CSV HEADER;
   `,
 };
 
@@ -98,34 +92,6 @@ const addSeedDataQueries = {
   `,
 };
 
-// Connect first to the postgres database to manage inventory database drop/create
-let pool = new Pool({
-  connectionString: pgConnection,
-});
-
-// Drop the existing inventory database and create a new one
-pool.query(`DROP DATABASE IF EXISTS ${database}`)
-  .then(() => pool.query(`CREATE DATABASE ${database}`))
-  .then(() => pool.end())
-  // Connect to the inventory database to create local inventory store tables
-  .then(() => {
-    pool = new Pool({ connectionString: dbConnection });
-    return pool.query(createTableQueries.listings);
-  })
-  .then(() => pool.query(createTableQueries.availability))
-  // Create temp tables for CSV imports
-  .then(() => pool.query(createTableQueries.listingsRawData))
-  .then(() => pool.query(createTableQueries.availabilityRawData))
-  .then(() => pool.query(csvImportQueries.listings))
-  .then(() => pool.query(csvImportQueries.availability))
-  // Add seed data to inventory store tables
-  .then(() => pool.query(addSeedDataQueries.listings))
-  .then(() => pool.query(addSeedDataQueries.availability))
-  .then(() => pool.end())
-  .then(() => {
-    console.log('INVENTORY STORE: All tables created and seeded with data.');
-  })
-  .catch((err) => {
-    console.error(err);
-    pool.end();
-  });
+module.exports.createTableQueries = createTableQueries;
+module.exports.csvImportQueries = csvImportQueries;
+module.exports.addSeedDataQueries = addSeedDataQueries;
