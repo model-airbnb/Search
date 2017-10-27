4803c5c66031e45fd845ac32500091e5bc461f07
christine-w <christine.wong@gmail.com>
2017-10-27 16:31:36 -0700

    Add http endpoint to support search by market and date range

--- server/httpSearch.js
+++ server/httpSearch.js
@@ -21,6 +21,17 @@ const createService = (inventoryStore) => {
       .catch(console.error);
   });
 
+  service.get('/search/:market/:checkin/:checkout/:limit', (req, res) => {
+    const {
+      market, checkin, checkout, limit,
+    } = req.params;
+    inventoryStore.getAvailableListings(market, checkin, checkout, limit)
+      .then((listings) => {
+        res.status(200).send(listings);
+      })
+      .catch(console.error);
+  });
+
   return service;
 };
 
