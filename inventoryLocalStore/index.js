3bccb1449ea9790c79cadea2b6fc2409fa7d1c0f
christine-w <christine.wong@gmail.com>
2017-10-26 21:44:11 -0700

    Retrieve available listings from the db by market and date range

--- inventoryLocalStore/index.js
+++ inventoryLocalStore/index.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
 
+const msToDays = ms => ms / 86400000;
+
 class Inventory {
   constructor(connection) {
     this.pool = new Pool({
@@ -10,6 +12,30 @@ class Inventory {
   getListings(market, limit) {
     const queryLimit = limit ? ` FETCH FIRST ${limit} ROWS ONLY` : '';
     const queryString = `SELECT * FROM listings WHERE market = '${market}'${queryLimit}`;
+    return this.pool.query(queryString)
+      .then(result => result.rows)
+      .catch(console.error);
+  }
+
+  getAvailableListings(market, start, end, limit) {
+    const lengthOfStay = msToDays(Date(end) - Date(start));
+    const queryLimit = limit ? ` FETCH FIRST ${limit * lengthOfStay} ROWS ONLY` : '';
+    const queryString = `
+      SELECT DISTINCT *
+      FROM listings l
+      INNER JOIN availability a ON l.id = a.listing_id
+      INNER JOIN (
+        SELECT  listing_id,
+                count(listing_id) num_nights
+        FROM availability
+        WHERE inventory_date BETWEEN '${start}'::timestamp AND '${end}'::timestamp
+        GROUP BY listing_id
+      ) s  ON l.id = s.listing_id
+      WHERE num_nights = (SELECT DATE_PART('day', '${end}'::timestamp - '${start}'::timestamp) + 1)
+        AND inventory_date BETWEEN '${start}'::timestamp AND '${end}'::timestamp
+        AND l.market = '${market}'
+        ${queryLimit}
+    `;
     return this.pool.query(queryString)
       .then(result => result.rows)
       .catch(console.error);
