36324b15e07819a7c53063026cc702c5f09c947c
christine-w <christine.wong@gmail.com>
2017-10-27 22:22:19 -0700

    Add unit tests for search by market and date range server routes

--- server/httpSearch.js
+++ server/httpSearch.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const getStayBookendNights = (params) => {
+  const lastNight = new Date(params.checkout);
+  lastNight.setDate(lastNight.getDate() - 1);
+  return { firstNight: params.checkin, lastNight: lastNight.toJSON().split('T')[0] };
+};
+
 const createService = (inventoryStore) => {
   const service = express();
 
@@ -21,11 +27,20 @@ const createService = (inventoryStore) => {
       .catch(console.error);
   });
 
+  service.get('/search/:market/:checkin/:checkout', (req, res) => {
+    const { market } = req.params;
+    const { firstNight, lastNight } = getStayBookendNights(req.params);
+    inventoryStore.getAvailableListings(market, firstNight, lastNight)
+      .then((listings) => {
+        res.status(200).send(listings);
+      })
+      .catch(console.error);
+  });
+
   service.get('/search/:market/:checkin/:checkout/:limit', (req, res) => {
-    const {
-      market, checkin, checkout, limit,
-    } = req.params;
-    inventoryStore.getAvailableListings(market, checkin, checkout, limit)
+    const { market, limit } = req.params;
+    const { firstNight, lastNight } = getStayBookendNights(req.params);
+    inventoryStore.getAvailableListings(market, firstNight, lastNight, limit)
       .then((listings) => {
         res.status(200).send(listings);
       })
