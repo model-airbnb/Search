11d2fbad064d8658ab985c1abf445879750f7efb
christine-w <christine.wong@gmail.com>
2017-10-28 14:21:44 -0700

    Prototype publishing search query messages with Amazon SQS

--- server/httpSearch.js
+++ server/httpSearch.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const uniqid = require('uniqid');
+const messageBus = require('./amazonSQS');
 
 const getStayBookendNights = (params) => {
   const lastNight = new Date(params.checkout);
@@ -6,10 +8,30 @@ const getStayBookendNights = (params) => {
   return { firstNight: params.checkin, lastNight: lastNight.toJSON().split('T')[0] };
 };
 
+const searchQueryMessage = (params) => {
+  const {
+    visitId, userId, market, checkin, checkout, roomType,
+  } = params;
+
+  const searchQueryId = uniqid('search-');
+  const messagePayload = {
+    searchQueryId,
+    timestamp: new Date(),
+    visitId,
+    userId,
+    market,
+    checkIn: new Date(checkin),
+    checkOut: new Date(checkout),
+    roomType: roomType || 'any',
+  };
+
+  return { searchQueryId, messagePayload };
+};
+
 const createService = (inventoryStore) => {
   const service = express();
 
-  service.get('/search/:market', (req, res) => {
+  service.get('/search/:visitId/:userId/:market', (req, res) => {
     const { market } = req.params;
     inventoryStore.getListings(market)
       .then((listings) => {
@@ -18,7 +40,7 @@ const createService = (inventoryStore) => {
       .catch(console.error);
   });
 
-  service.get('/search/:market/:limit', (req, res) => {
+  service.get('/search/:visitId/:userId/:market/:limit', (req, res) => {
     const { market, limit } = req.params;
     inventoryStore.getListings(market, limit)
       .then((listings) => {
@@ -27,7 +49,7 @@ const createService = (inventoryStore) => {
       .catch(console.error);
   });
 
-  service.get('/search/:market/:checkin/:checkout', (req, res) => {
+  service.get('/search/:visitId/:userId/:market/:checkin/:checkout', (req, res) => {
     const { market } = req.params;
     const { firstNight, lastNight } = getStayBookendNights(req.params);
     inventoryStore.getAvailableListings(market, firstNight, lastNight)
@@ -37,14 +59,16 @@ const createService = (inventoryStore) => {
       .catch(console.error);
   });
 
-  service.get('/search/:market/:checkin/:checkout/:limit', (req, res) => {
+  service.get('/search/:visitId/:userId/:market/:checkin/:checkout/:limit', (req, res) => {
     const { market, limit } = req.params;
     const { firstNight, lastNight } = getStayBookendNights(req.params);
+    const { messagePayload } = searchQueryMessage(req.params);
     inventoryStore.getAvailableListings(market, firstNight, lastNight, limit)
       .then((listings) => {
         res.status(200).send(listings);
       })
       .catch(console.error);
+    messageBus.publish({ payload: messagePayload }, 'searchQuery');
   });
 
   return service;
