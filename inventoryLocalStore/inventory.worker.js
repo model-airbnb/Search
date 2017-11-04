519e17f37d48e6ac68f0593c8d6763fcd52a2329
christine-w <christine.wong@gmail.com>
2017-11-03 23:47:00 -0700

    Implement worker script to process inventory and availability update messages

---
+++ inventoryLocalStore/inventory.worker.js
@@ -0,0 +1,56 @@
+const { inventoryInbox, checkForMessages } = require('../messageBus/index');
+const inventoryDbConnection = require('./config').dbConnection;
+const Inventory = require('./index');
+
+const inventoryStore = new Inventory(inventoryDbConnection);
+
+const MAX_WORKERS = process.argv[2] || 1;
+const TOPIC_INVENTORY = 'Inventory';
+const TOPIC_AVAILABILITY = 'Availability';
+const UPDATE_TYPE_ADD = 'ADD';
+const UPDATE_TYPE_REMOVE = 'REMOVE';
+const SLEEP_MS = 2000;
+
+const processInventoryUpdate = message => inventoryStore.addOrUpdateListing(message);
+
+const processAvailabilityUpdate = (message) => {
+  const availabilityUpdateHandlers = {
+    [UPDATE_TYPE_ADD]: inventoryStore.addOrUpdateAvailability,
+    [UPDATE_TYPE_REMOVE]: inventoryStore.deleteAvailability,
+  };
+  const availabilityUpdates = message.date.map((inventoryDate, index) => {
+    const update = {
+      listingId: message.listingId,
+      inventoryDate,
+      price: message.price[index],
+    };
+    return availabilityUpdateHandlers[message.updateType](update);
+  });
+  return Promise.all(availabilityUpdates);
+};
+
+const messageHandlers = {
+  [TOPIC_INVENTORY]: processInventoryUpdate,
+  [TOPIC_AVAILABILITY]: processAvailabilityUpdate,
+};
+
+const processInventoryEvents = (id) => {
+  const taskTimeStart = Date.now();
+  let messages;
+  checkForMessages(inventoryInbox)
+    .then((messageBatch) => {
+      messages = messageBatch;
+      const dbUpdates = messages.map(message =>
+        messageHandlers[message.topic](message.payload) || Promise.resolve());
+      return Promise.all(dbUpdates);
+    })
+    .then(() => {
+      setTimeout(processInventoryEvents.bind(this, id), SLEEP_MS);
+      console.log(`${id}, messages processed: ${messages.length}, time lapsed: ${Date.now() - taskTimeStart}`);
+    })
+    .catch(console.error);
+};
+
+for (let i = 1; i <= MAX_WORKERS; i += 1) {
+  processInventoryEvents(i);
+}
